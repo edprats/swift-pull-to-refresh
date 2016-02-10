b7e4cd22acdb9f1545808eaefcf7164816f9b3b9
Ed Prats <edprats@gmail.com>
2016-02-09 21:05:51 -0800

    Initial Commit

---
+++ Pull To Refresh Table View/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Pull To Refresh Table View
+//
+//  Created by Eduardo Prats on 2/9/16.
+//  Copyright © 2016 edprats. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
